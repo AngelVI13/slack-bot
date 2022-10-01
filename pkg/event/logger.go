53fc63b92ddb881245f4629df8ef6940a8cf104d
Angel VI <a.v.iliev13@gmail>
2022-10-01 14:57:04 +0300

    remove log padding

--- pkg/event/logger.go
+++ pkg/event/logger.go
@@ -4,24 +4,13 @@ import (
 	"log"
 )
 
-const (
-	LogPadding = 30
-)
-
 type EventLogger struct {
-	padding int
 }
 
 func NewEventLogger() *EventLogger {
-	return &EventLogger{
-		padding: LogPadding,
-	}
+	return &EventLogger{}
 }
 
 func (ev *EventLogger) Consume(event Event) {
-	log.Printf(
-		"%*s",
-		ev.padding,
-		event,
-	)
+	log.Printf("\t%s", event)
 }
