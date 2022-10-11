056e0a897a333c3dce4cb9ceec63e1b30237e93a
Angel VI <a.v.iliev13@gmail>
2022-10-11 08:38:03 +0300

    refactor release logic to separate file

---
+++ pkg/parking/release.go
@@ -0,0 +1,169 @@
+package parking
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/AngelVI13/slack-bot/pkg/common"
+)
+
+type ReleaseInfo struct {
+	ReleaserId string
+	OwnerId    string
+	OwnerName  string
+	Space      *ParkingSpace
+	StartDate  *time.Time
+	EndDate    *time.Time
+	Submitted  bool
+	Cancelled  bool
+
+	// These are only used while the user is choosing date range to refer
+	// between space selected and release range selected (i.e. between booking modal
+	// and corresponding release modal)
+	RootViewId string
+	ViewId     string
+}
+
+func (i *ReleaseInfo) MarkSubmitted() {
+	log.Printf("ReleaseInfo Submitted: %v", i)
+	i.Submitted = true
+
+	// Need to reset view IDs as they are no longer needed.
+	// If we don't reset them and user tries to release another
+	// space without closing the parent model -> GetByViewId can return
+	// incorrect data.
+	i.RootViewId = ""
+	i.ViewId = ""
+}
+
+func (i *ReleaseInfo) MarkCancelled() {
+	log.Printf("ReleaseInfo Cancelled: %v", i)
+	i.Cancelled = true
+}
+
+func (i *ReleaseInfo) DataPresent() bool {
+	return (i.ReleaserId != "" &&
+		i.OwnerId != "" &&
+		i.OwnerName != "" &&
+		i.Space != nil &&
+		i.StartDate != nil &&
+		i.EndDate != nil)
+}
+
+func (i *ReleaseInfo) Check() string {
+	if !i.DataPresent() {
+		return fmt.Sprintf("Missing date information for temporary release of space (%d)", i.Space.Number)
+	}
+
+	return common.CheckDateRange(*i.StartDate, *i.EndDate)
+}
+
+func (i ReleaseInfo) String() string {
+	startDateStr := "nil"
+	if i.StartDate != nil {
+		startDateStr = i.StartDate.Format("2006-01-02")
+	}
+
+	endDateStr := "nil"
+	if i.EndDate != nil {
+		endDateStr = i.EndDate.Format("2006-01-02")
+	}
+
+	return fmt.Sprintf(
+		"ReleaseInfo(space=%d, userName=%s, start=%s, end=%s)",
+		i.Space.Number,
+		i.OwnerName,
+		startDateStr,
+		endDateStr,
+	)
+}
+
+type ReleaseMap map[int]*ReleaseInfo
+
+func (q ReleaseMap) Get(space int) *ReleaseInfo {
+	releaseInfo, ok := q[space]
+	if !ok {
+		return nil
+	}
+	return releaseInfo
+}
+
+func (q ReleaseMap) GetByReleaserId(userId string) *ReleaseInfo {
+	for _, item := range q {
+		if item.ReleaserId == userId {
+			return item
+		}
+	}
+	return nil
+}
+
+func (q ReleaseMap) GetByRootViewId(rootId string) *ReleaseInfo {
+	for _, item := range q {
+		if item.RootViewId == rootId {
+			return item
+		}
+	}
+	return nil
+}
+
+func (q ReleaseMap) GetByViewId(viewId string) *ReleaseInfo {
+	for _, item := range q {
+		if item.ViewId == viewId {
+			return item
+		}
+	}
+	return nil
+}
+
+func (q ReleaseMap) Remove(space int) bool {
+	_, ok := q[space]
+	if !ok {
+		return false
+	}
+
+	log.Println("Removing from release map: space ", space)
+	delete(q, space)
+	return true
+}
+
+func (q ReleaseMap) RemoveByViewId(viewId string) (int, bool) {
+	spaceNum := -1
+	for space, info := range q {
+		if info.ViewId == viewId {
+			spaceNum = space
+			break
+		}
+	}
+	if spaceNum == -1 {
+		return spaceNum, false
+	}
+
+	log.Println("Removing from release map: space ", spaceNum)
+	delete(q, spaceNum)
+	return spaceNum, true
+}
+
+func (q ReleaseMap) Add(
+	viewId,
+	releaserId,
+	ownerName,
+	ownerId string,
+	space *ParkingSpace,
+) (*ReleaseInfo, error) {
+	if q.Get(space.Number) != nil {
+		return nil, fmt.Errorf("Space %d already marked for release.", space.Number)
+	}
+
+	releaseInfo := &ReleaseInfo{
+		RootViewId: viewId,
+		ReleaserId: releaserId,
+		OwnerName:  ownerName,
+		OwnerId:    ownerId,
+		Space:      space,
+		Submitted:  false,
+	}
+
+	q[space.Number] = releaseInfo
+	return releaseInfo, nil
+}
