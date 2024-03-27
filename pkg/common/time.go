b2aa501d72c4a21cf8e695c9f22f9af2f1d9cc2e
AngelVI <a.v.iliev13@gmail.com>
2024-03-27 09:47:22 +0200

    timezone issue in check date range

--- pkg/common/time.go
+++ pkg/common/time.go
@@ -7,7 +7,16 @@ import (
 
 func CheckDateRange(start, end time.Time) string {
 	today := time.Now()
-	todayDate := time.Date(today.Year(), today.Month(), today.Day(), 0, 0, 0, 0, time.UTC)
+	todayDate := time.Date(
+		today.Year(),
+		today.Month(),
+		today.Day(),
+		0,
+		0,
+		0,
+		0,
+		today.Location(),
+	)
 
 	if start.Before(todayDate) {
 		return fmt.Sprintf("Start date is in the past: %s", start.Format("2006-01-02"))
@@ -25,5 +34,6 @@ func CheckDateRange(start, end time.Time) string {
 }
 
 func EqualDate(date1, date2 time.Time) bool {
-	return date1.Year() == date2.Year() && date1.Month() == date2.Month() && date1.Day() == date2.Day()
+	return date1.Year() == date2.Year() && date1.Month() == date2.Month() &&
+		date1.Day() == date2.Day()
 }
